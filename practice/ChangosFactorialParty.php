0e2dca010d044d751c4729b3708ce8cc5402c814
Facundo <isifc.facu@gmail.com>
2014-08-28 20:02:58 -0300

    factorial

--- practice/ChangosFactorialParty.php
+++ practice/ChangosFactorialParty.php
@@ -1,8 +1,19 @@
-<?php
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            function fac($numero)
+            {
+            for ($index = 1;$index <= $numero;$index++) {
+                 $elchangonum*= $index;
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+                }
+                return $elchangonum;
+            }
+        
+        ?>
+    </body>
+</html>
